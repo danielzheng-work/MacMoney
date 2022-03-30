85a0a7f9421a92178fbd49a8f5c6949393e0ff24
danielzheng-work <zhengc15@mcmaster.ca>
2022-03-30 02:21:16 -0400

    basic flows completed

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# MacMoney
+<p align="center" style="color: #FF7262; font-weight: 1000; font-size: 1.75rem;">
+  MacMoney
+</p>
+
+<div align="center">
+  <p float="left">
+  <img src="https://github.com/danielzheng-work/MacMoney/blob/main/img/home.jpg" width="48%" />
+  <img src="https://github.com/danielzheng-work/MacMoney/blob/main/img/game.jpg" width="48%" />
+</p>
+</div>
+
+# About
+This online Tutorial website is based on the idea of teaching students how to manage their money in some pleasant, interactive way.
+
+## How to run
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) 
+In the project directory, you can run:
+
+`npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+## other Contributors:
+
+* bilaval
+* skiddie
+* sharmb9
+
+
---
+++ src/components/bilaval/QuestionBox.jsx
@@ -0,0 +1,109 @@
+import React, { useState,useEffect } from "react";
+import QFBtext from './QuestionFeedback';
+import questions from './questions';
+import Npc from '../skiddie/Npc';
+import PopUpButton from '../skiddie/PopUpButton';
+import PopUp from '../skiddie/PopUp';
+import Ntext from '../danielzheng/NPC_text';
+import End from '../skiddie/End';
+
+const QuestionBox = () => {
+
+
+  //   setting current question to 0 means starting with first question
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [showQFeedback, setShowQFeedback] = useState();
+
+  const [feedback, setfeedback] = useState({message: ''});
+
+  const [selectedOption, setSelectedOption] = useState([]);
+
+  const [showTopic, setShowTopic] = useState();
+
+  const [isEnd, setIsEnd] = useState();
+
+  const SELECT_OPTIONS_KEY = "SelectedOptions"
+  
+  let localStorageAnswers;
+  
+  //   shows the feedback when an option is clicked and (TODO) saves selected option to storage
+  function handleOptionClick(option) {
+    setShowQFeedback(true);
+    console.log("showFeddback is true")
+    const newfeedback = {message: option.feedback};
+    setfeedback(newfeedback);
+    //   saves the option to local storage
+    setSelectedOption([...selectedOption, option])
+    localStorage.setItem(SELECT_OPTIONS_KEY,JSON.stringify(selectedOption));
+    console.log("handleOption",currentQuestion)
+    // alert(localStorage.getItem(SELECT_OPTIONS_KEY))
+    
+   };
+      
+      
+  // };
+
+  function handleNextClick(){
+    console.log("next click",currentQuestion)
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length){
+        setCurrentQuestion(nextQuestion)
+    }
+    if (questions[currentQuestion].isLast==true) {
+      setShowTopic(true)
+      console.log("isLast is true")
+    }
+
+    if (questions[currentQuestion].EOF==true) {
+      setIsEnd(true)
+      console.log("isEnd is true")
+    }
+    // on clicking next on topic, set showTopic to false
+    setShowQFeedback(false);
+    
+    
+};
+
+  const setTopicFalse= () => {
+    setShowTopic(false)
+    console.log("next click",currentQuestion)
+  }
+
+  return (
+    <>
+    {/* render topic component here */}
+      
+      {!isEnd && !showTopic && !showQFeedback &&<div className="question-container App-question">
+        <div className="question-section">
+          <div className="question-text">
+            {questions[currentQuestion].questionText}
+          </div>
+          {/* This is just a placeholder button, handleNextClick will be used on feedback pop up */}
+          {/* <div className = "option-section"><button className="option-button" onClick={() => handleNextClick()}>
+              Next
+          </button></div> */}
+        </div>
+        <div className="option-section">
+          {questions[currentQuestion].answerOptions.map((answerOption) => (
+            <button
+              onClick={() => handleOptionClick(answerOption)}
+              className="option-button"
+            >
+              {answerOption.answerText}
+            </button>
+          ))}
+        </div>
+        <Npc target="#hint"/>
+        <PopUp id="hint">
+          {questions[currentQuestion].hint}
+        </PopUp>
+      </div>}
+      {!isEnd && showTopic && <Ntext txt = {questions[currentQuestion].topic}  action= {setTopicFalse}/> }
+      {showQFeedback &&<><Npc/>< QFBtext text= {feedback.message} action={handleNextClick} /> </>}
+      {isEnd &&<End/>}
+    </>
+  );
+};
+
+export default QuestionBox;
